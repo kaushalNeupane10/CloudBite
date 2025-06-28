15cb797527152f97b7976cd0b7af1941b55d39c0
Nebulanook <kaushal98211@gmail.com>
2025-06-28 12:39:52 +0545

    improving cloudinary img to use https over http for security reasons.

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from .models import MenuItem, CartItem, Order, Review, OrderItem 
+from .models import MenuItem, CartItem, Order, Review, OrderItem
 from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ["id", "username"]
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
@@ -14,21 +14,24 @@ class MenuItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'description', 'price', 'image']
+        fields = ["id", "title", "description", "price", "image"]
 
-    def get_image(self, obj):
-        return obj.image.url if obj.image else None
+
+def get_image(self, obj):
+    if obj.image:
+        return obj.image.url.replace("http://", "https://")
+    return None
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     menu_item = MenuItemSerializer(read_only=True)
     menu_item_id = serializers.PrimaryKeyRelatedField(
-        queryset=MenuItem.objects.all(), write_only=True, source='menu_item'
+        queryset=MenuItem.objects.all(), write_only=True, source="menu_item"
     )
 
     class Meta:
         model = CartItem
-        fields = ['id', 'menu_item', 'menu_item_id', 'quantity']
+        fields = ["id", "menu_item", "menu_item_id", "quantity"]
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -36,10 +39,10 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = "__all__"
 
     def create(self, validated_data):
-        validated_data['user'] = self.context['request'].user
+        validated_data["user"] = self.context["request"].user
         return super().create(validated_data)
 
 
@@ -48,7 +51,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = OrderItem
-        fields = ['id', 'menu_item', 'quantity', 'price_at_order']
+        fields = ["id", "menu_item", "quantity", "price_at_order"]
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -57,7 +60,15 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['id', 'user', 'order_items', 'total_price', 'is_paid', 'status', 'ordered_at']
+        fields = [
+            "id",
+            "user",
+            "order_items",
+            "total_price",
+            "is_paid",
+            "status",
+            "ordered_at",
+        ]
         read_only_fields = fields  # Make all fields read-only
 
 
@@ -66,11 +77,11 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ["username", "email", "password"]
 
     def create(self, validated_data):
         return User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password']
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"],
         )
